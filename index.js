f1309a5fa741c732c2675a741d973faabca2f79b
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-24 22:15:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,51 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+
+/*function split(string){
+  return string.split(' ');
+}
+newTitleCased = tutorials.map(split);
+console.log(newTitleCased);
+
+function join(string){
+  return string.join(' ');
+}
+newTitleCased.map(join);
+console.log(newTitleCased);*/
+
+//i have seen something difficult to achieve
+function capitalizeTheFirstLetterOfEachWord(words){
+  let separateWord = words.split(' '); //forget about the to lower case part
+  for(let i=0; i<separateWord.length; i++){
+    separateWord[i] = separateWord[i].charAt(0).toUpperCase() + separateWord[i].substring(1);
+  }
+  return separateWord.join(' ');
+  
+}
+console.log(capitalizeTheFirstLetterOfEachWord('this is crazy'));
+
+//now to master how this join and charAt work
+let test ='this is a test run'.split(' ');//so split creates a new array with the strings
+
+console.log(test);
+  
+console.log(test.join(' '))
+console.log(test); //it appears join is non-destructive
+//i have not grasped this joining of toLowerCase and charAt
+//is there like an order i think there is.you write them based on the order in which you want it to be executed
+//split is destructive
+
+
+/*const newTutorials = tutorials.map(capitalizeTheFirstLetterOfEachWord);
+console.log(newTutorials);
+console.log(titleCased());
+
+function titleCased(){
   return tutorials
 }
+//nitamalizia baadaye */
+function titleCased(){
+ return tutorials.map(capitalizeTheFirstLetterOfEachWord)
+}
+console.log(titleCased())
